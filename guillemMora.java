38206fbb45815f6679241bb99ebf32ab08589715
bk203esd <bk203esd@gmail.com>
2020-05-01 18:08:41 +0200

    1r comit constructors

---
+++ guillemMora.java
@@ -0,0 +1,29 @@
+import java.util.Objects;
+
+public class guillemMora {
+    private int edat;
+    private String tel;
+
+    public guillemMora() {
+    }
+
+    public String toString() {
+        return "guillemMora{" +
+                "edat=" + edat +
+                ", tel='" + tel + '\'' +
+                '}';
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        guillemMora that = (guillemMora) o;
+        return edat == that.edat &&
+                Objects.equals(tel, that.tel);
+    }
+
+    guillemMora(int e, String t) {
+        edat = e;
+        tel = t;
+    }
+}
